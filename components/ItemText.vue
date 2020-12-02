343a331afbc1cda8eeb479c7a3b099396159a124
Dries Bos <driesbos@MBP-van-Dries.home>
2020-12-02 11:32:56 +0100

    update itemText

--- components/ItemText.vue
+++ components/ItemText.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-editable="blok" class="item item-Text">
-    <div class="item-Text_Container item-Container" :class="blok.size">
+    <div class="item-Text_Container item-Container medium">
       <div class="item-Text_Single item-Single">
         <LazyMarkdown :input="blok.text" />
       </div>
@@ -18,17 +18,17 @@ export default {
 
 <style lang="sass">
 .item-Text
-  margin-top: 1rem
-  margin-bottom: 1rem
-  &_Container
-    &.small
-      .item-Text_Single
-        padding-right: 1em
-    &.large
-      .item-Text_Single
-        padding-right: 1em
-    &.full
-      .item-Text_Single
-        padding-left: 1em
-        padding-right: 1em
+  margin-top: var(--spacing-vertical)
+  margin-bottom: var(--spacing-vertical)
+  // &_Container
+  //   &.small
+  //     .item-Text_Single
+  //       padding-right: 1em
+  //   &.large
+  //     .item-Text_Single
+  //       padding-right: 1em
+  //   &.full
+  //     .item-Text_Single
+  //       padding-left: 1em
+  //       padding-right: 1em
 </style>
