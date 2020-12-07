bd7b6981dad69d769e89381c24571c2f8d3aef30
Dries Bos <driesbos@MBP-van-Dries.home>
2020-12-07 20:54:33 +0100

    fix: text

--- components/ItemText.vue
+++ components/ItemText.vue
@@ -1,7 +1,9 @@
 <template>
   <div v-editable="blok" class="item item-NonImage item-Text">
     <div class="item_Container item-NonImage_Container item-Text_Container">
-      <div class="item_Single item-NonImage_Single item-Text_Single"></div>
+      <div class="item_Single item-NonImage_Single item-Text_Single">
+        <LazyMarkdown :input="blok.text" />
+      </div>
     </div>
   </div>
 </template>
