505986c7490ef43f08d156e4c80894d87798c54e
DriesBos <info@driesbos.com>
2021-01-29 08:26:00 +0000

    videoplayer added

--- plugins/components.js
+++ plugins/components.js
@@ -10,6 +10,7 @@ import ItemDataRow from "~/components/ItemDataRow.vue"
 import ItemImage from "~/components/ItemImage.vue"
 import ItemSound from "~/components/ItemSound.vue"
 import ItemText from "~/components/ItemText.vue"
+import ItemVideo from "~/components/ItemVideo.vue"
 import ItemVideoVimeo from "~/components/ItemVideoVimeo.vue"
 import ItemVideoYoutube from "~/components/ItemVideoYoutube.vue"
 // import ItemVideoplayer from "~/components/ItemVideoplayer.vue"
@@ -24,6 +25,7 @@ Vue.component("blok-item-data-row", ItemDataRow)
 Vue.component("blok-item-image", ItemImage)
 Vue.component("blok-item-sound", ItemSound)
 Vue.component("blok-item-text", ItemText)
+Vue.component("blok-item-video", ItemVideo)
 Vue.component("blok-item-video-vimeo", ItemVideoVimeo)
 Vue.component("blok-item-video-youtube", ItemVideoYoutube)
 // Vue.component("blok-item-videoplayer", ItemVideoplayer)
